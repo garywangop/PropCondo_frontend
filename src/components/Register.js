300c0960656ccda095bcd9f1d3db12808142bbc5
garywangop <garywangop@gmail.com>
2020-03-07 03:02:48 -0500

    Complete register.

--- src/components/Register.js
+++ src/components/Register.js
@@ -7,34 +7,76 @@ import {
     Cascader,
     Select,
     Button,
+    message,
 } from 'antd';
+import {REGISTER} from '../constants';
 
 const { Option } = Select;
 
 const residences = [
     {
-        value: 'Place 1',
-        label: 'Street 1',
+        value: 'Place1',
+        label: 'A',
         children: [
             {
-                value: 'Apt1',
-                label: 'Apt 1',
+                value: 'A1',
+                label: '001',
+            },
+            {
+                value: 'A2',
+                label: '002',
+            },
+            {
+                value: 'A3',
+                label: '003',
             },
         ],
     },
     {
-        value: 'Place 2',
-        label: 'Street 2',
+        value: 'Place2',
+        label: 'B',
         children: [
             {
-                value: 'Building1',
-                label: 'Building 1',
-                children: [
-                    {
-                        value: 'Apt2',
-                        label: 'Apt 2',
-                    },
-                ],
+                value: 'B1',
+                label: '001',
+            },
+            {
+                value: 'B2',
+                label: '002',
+            },
+            {
+                value: 'B3',
+                label: '003',
+            },
+            {
+                value: 'B4',
+                label: '004',
+            },
+        ],
+    },
+    {
+        value: 'Place3',
+        label: 'C',
+        children: [
+            {
+                value: 'C1',
+                label: '001',
+            },
+            {
+                value: 'C2',
+                label: '002',
+            },
+            {
+                value: 'C3',
+                label: '003',
+            },
+            {
+                value: 'C4',
+                label: '004',
+            },
+            {
+                value: 'C5',
+                label: '005',
             },
         ],
     },
@@ -48,9 +90,39 @@ class RegistrationForm extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
+        let lastResponse;
         this.props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                fetch(`${REGISTER}`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                        // 'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    body: JSON.stringify({
+                        user_id: 0,
+                        username: values.username,
+                        password: values.password,
+                        roomId: values.roomId[1],
+                        email: values.email,
+                        firstName: values.firstName,
+                        lastName: values.lastName,
+                        admin: false,
+                    }),
+                }).then((response) => {
+                    lastResponse = response;
+                    return response.text();
+                }, (error) => {
+                    console.log('Error' + error);
+                }).then((text) => {
+                    if (lastResponse.ok) {
+                        message.success(text);
+                        this.props.history.push('/dashboard');
+                    } else {
+                        message.error(text);
+                    }
+                });
             }
         });
     };
@@ -107,6 +179,11 @@ class RegistrationForm extends Component {
 
         return (
             <Form onSubmit={this.handleSubmit} className="register-form">
+                <Form.Item label="Username">
+                    {getFieldDecorator('username', {
+                        rules: [{ required: true, message: 'Please input your username!', whitespace: true }],
+                    })(<Input />)}
+                </Form.Item>
                 <Form.Item label="E-mail">
                     {getFieldDecorator('email', {
                         rules: [
@@ -147,23 +224,9 @@ class RegistrationForm extends Component {
                         ],
                     })(<Input.Password onBlur={this.handleConfirmBlur} />)}
                 </Form.Item>
-                <Form.Item
-                    label={
-                        <span>
-              Nickname&nbsp;
-                            <Tooltip title="What do you want others to call you?">
-                <Icon type="question-circle-o" />
-              </Tooltip>
-            </span>
-                    }
-                >
-                    {getFieldDecorator('nickname', {
-                        rules: [{ required: true, message: 'Please input your nickname!', whitespace: true }],
-                    })(<Input />)}
-                </Form.Item>
                 <Form.Item label="Apartment Number">
-                    {getFieldDecorator('residence', {
-                        initialValue: ['Place1', 'Place2', 'Place3'],
+                    {getFieldDecorator('roomId', {
+                        // initialValue: ['Place1', 'Place2', 'Place3'],
                         rules: [
                             { type: 'array', required: true, message: 'Please select your habitual residence!' },
                         ],
@@ -173,6 +236,16 @@ class RegistrationForm extends Component {
                     {getFieldDecorator('phone', {
                         rules: [{ required: true, message: 'Please input your phone number!' }],
                     })(<Input style={{ width: '100%' }} />)}
+                </Form.Item>
+                <Form.Item label="First Name">
+                    {getFieldDecorator('firstName', {
+                        rules: [{required: true, message: 'Please input your first name!'}]
+                    })(<Input />)}
+                </Form.Item>
+                <Form.Item label="Last Name">
+                    {getFieldDecorator('lastName', {
+                        rules: [{required: true, message: 'Please input your first name!'}]
+                    })(<Input />)}
                 </Form.Item>
                 <Form.Item>
                     <Button type="primary" htmlType="submit">
--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,12 @@
 // http://34.237.218.116:8080/PropCondo/api/comments/1       //use this link to get first comment
 // http://34.237.218.116:8080/PropCondo/api/posts            //use this link to get posts info
 // http://34.237.218.116:8080/PropCondo/api/posts/1          //use this link to get first post
+/*
+http://34.237.218.116:8080/PropCondo/api/residents/      POST: register
+http://34.237.218.116:8080/PropCondo/api/residents/"username"    GET: get "username" info
+http://34.237.218.116:8080/PropCondo/api/residents/      GET: get all users info
+*/
 
 export const COMMENT = 'http://34.237.218.116:8080/PropCondo/api';
 export const POST = 'http://34.237.218.116:8080/PropCondo/api';
+export const REGISTER = 'http://34.237.218.116:8080/PropCondo/api/residents';
