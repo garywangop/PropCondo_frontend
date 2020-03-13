852f6835ef25fd9642e42dc499c3827dcbd63af0
Asuka09 <jjyycc0910@gmail.com>
2020-03-12 22:24:32 -0400

    DashBorad add buttons for redirect

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Tabs, Button } from 'antd';
 import Discussion from "./Discussion";
 import Calendar from "./Calender";
@@ -9,17 +9,42 @@ const { TabPane } = Tabs;
 const operations = <Button>Hello xxx</Button>;
 
 class DashBoard extends Component {
+
+    state = {
+        activeTab: "1"
+    };
+
+    changeTab = activeKey => {
+        console.log(activeKey);
+        this.setState({
+            activeTab: activeKey
+        });
+    };
+
+
     render() {
         return (
-            <Tabs tabBarExtraContent={operations} className="main-tabs">
+            <Tabs tabBarExtraContent={operations} className="main-tabs"
+                activeKey={this.state.activeTab} onChange={this.changeTab}>
                 <TabPane tab="Home" key="1">
-                    Notice section and user function bar in this tab
+                    <div>
+                        welcome back!
+                        <div>
+                            <Button type="default" size="large" onClick={() => this.changeTab("2")}>
+                                Discussion
+                            </Button>
+                            <Button type="default" size="large" onClick={() => this.changeTab("3")}>
+                                Reservation
+                            </Button>
+                        </div>
+                    </div>
+
                 </TabPane>
                 <TabPane tab="Discussion" key="2">
-                    <Discussion/>
+                    <Discussion />
                 </TabPane>
                 <TabPane tab="Reservation" key="3">
-                    <Calendar/>
+                    <Calendar />
                 </TabPane>
                 <TabPane tab="Documents" key="4">
                     Content of tab 4
