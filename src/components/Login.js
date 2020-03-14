8d75bb3ca19d64cd16d31cadc39a61e2f6e25fe7
issaquahsun <xyapple28@gmail.com>
2020-03-13 21:11:59 -0700

    updated UI styles in app register and login forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,76 +1,88 @@
-import React, {Component} from 'react';
-import { Form, Icon, Input, Button, message } from 'antd';
-import { REGISTER } from '../constants';
+import React, { Component } from "react";
+import { Form, Icon, Input, Button, message } from "antd";
+import { REGISTER } from "../constants";
 
 class NormalLoginForm extends Component {
-    handleSubmit = e => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-                fetch(`${REGISTER}/${values.username}`, {
-                    method: 'GET',
-
-                }).then((response) => {
-                    if (response.ok) {
-                        return response.json();
-                    }
-                    throw new Error('Fail to login');
-                }, (error) => {
-                    console.log('Error', error);
-                }).then((text) => {
-                    console.log(text);
-                    if (text.password === values.password) {
-                        console.log('Able to login');
-                        message.success('Login success!');
-                        // this.props.history.push('./dashboard');
-                        this.props.handleLogin();
-                    } else {
-                        message.error('Login error, please login again!');
-                    }
-                });
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+        fetch(`${REGISTER}/${values.username}`, {
+          method: "GET"
+        })
+          .then(
+            response => {
+              if (response.ok) {
+                return response.json();
+              }
+              throw new Error("Fail to login");
+            },
+            error => {
+              console.log("Error", error);
             }
-        });
-    }
+          )
+          .then(text => {
+            console.log(text);
+            if (text.password === values.password) {
+              console.log("Able to login");
+              message.success("Login success!");
+              // this.props.history.push('./dashboard');
+              this.props.handleLogin();
+            } else {
+              message.error("Login error, please login again!");
+            }
+          });
+      }
+    });
+  };
 
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        return (
-            <div className="login-form">
-                <h1 className="login-text">User Log In</h1>
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Item>
-                        {getFieldDecorator('username', {
-                            rules: [{ required: true, message: 'Please input your username!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="Username"
-                            />,
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('password', {
-                            rules: [{ required: true, message: 'Please input your Password!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="Password"
-                            />,
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit">
-                            Log in
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </div>
-        );
-    }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className='login-form'>
+        <h1 className='login-text'>User Log In</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Item>
+            {getFieldDecorator("username", {
+              rules: [
+                { required: true, message: "Please input your username!" }
+              ]
+            })(
+              <Input
+                prefix={
+                  <Icon type='user' style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder='Username'
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator("password", {
+              rules: [
+                { required: true, message: "Please input your Password!" }
+              ]
+            })(
+              <Input
+                prefix={
+                  <Icon type='lock' style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                type='password'
+                placeholder='Password'
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button type='primary' htmlType='submit'>
+              Log In
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
 }
 
-const Login = Form.create({ name: 'normal_login' })(NormalLoginForm);
+const Login = Form.create({ name: "normal_login" })(NormalLoginForm);
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,259 +1,274 @@
-import React, {Component} from 'react';
-import {
-    Form,
-    Input,
-    Cascader,
-    Button,
-    message,
-} from 'antd';
-import {REGISTER} from '../constants';
+import React, { Component } from "react";
+import { Form, Input, Cascader, Button, message } from "antd";
+import { REGISTER } from "../constants";
 
 const residences = [
-    {
-        value: 'Place1',
-        label: 'A',
-        children: [
-            {
-                value: 'A1',
-                label: '001',
-            },
-            {
-                value: 'A2',
-                label: '002',
-            },
-            {
-                value: 'A3',
-                label: '003',
-            },
-        ],
-    },
-    {
-        value: 'Place2',
-        label: 'B',
-        children: [
-            {
-                value: 'B1',
-                label: '001',
-            },
-            {
-                value: 'B2',
-                label: '002',
-            },
-            {
-                value: 'B3',
-                label: '003',
-            },
-            {
-                value: 'B4',
-                label: '004',
-            },
-        ],
-    },
-    {
-        value: 'Place3',
-        label: 'C',
-        children: [
-            {
-                value: 'C1',
-                label: '001',
-            },
-            {
-                value: 'C2',
-                label: '002',
-            },
-            {
-                value: 'C3',
-                label: '003',
-            },
-            {
-                value: 'C4',
-                label: '004',
-            },
-            {
-                value: 'C5',
-                label: '005',
-            },
-        ],
-    },
+  {
+    value: "Place1",
+    label: "A",
+    children: [
+      {
+        value: "A1",
+        label: "001"
+      },
+      {
+        value: "A2",
+        label: "002"
+      },
+      {
+        value: "A3",
+        label: "003"
+      }
+    ]
+  },
+  {
+    value: "Place2",
+    label: "B",
+    children: [
+      {
+        value: "B1",
+        label: "001"
+      },
+      {
+        value: "B2",
+        label: "002"
+      },
+      {
+        value: "B3",
+        label: "003"
+      },
+      {
+        value: "B4",
+        label: "004"
+      }
+    ]
+  },
+  {
+    value: "Place3",
+    label: "C",
+    children: [
+      {
+        value: "C1",
+        label: "001"
+      },
+      {
+        value: "C2",
+        label: "002"
+      },
+      {
+        value: "C3",
+        label: "003"
+      },
+      {
+        value: "C4",
+        label: "004"
+      },
+      {
+        value: "C5",
+        label: "005"
+      }
+    ]
+  }
 ];
 
 class RegistrationForm extends Component {
-    state = {
-        confirmDirty: false,
-        autoCompleteResult: [],
-    };
+  state = {
+    confirmDirty: false,
+    autoCompleteResult: []
+  };
 
-    handleSubmit = e => {
-        e.preventDefault();
-        let lastResponse;
-        this.props.form.validateFieldsAndScroll((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-                fetch(`${REGISTER}`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                        // 'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                    body: JSON.stringify({
-                        user_id: 0,
-                        username: values.username,
-                        password: values.password,
-                        roomId: values.roomId[1],
-                        email: values.email,
-                        firstName: values.firstName,
-                        lastName: values.lastName,
-                        admin: false,
-                    }),
-                }).then((response) => {
-                    console.log('response: ', response);
-                    console.log('lastResponse: ', lastResponse);
-                    lastResponse = response;
-                    return response.text();
-                }, (error) => {
-                    console.log('Error' + error);
-                }).then((text) => {
-                    console.log('lastResponse in second then:' , lastResponse);
-                    if (lastResponse.ok) {
-                        message.success("Register successfully, please login");
-                    } else {
-                        message.error('User exists, please login!');
-                    }
-                });
+  handleSubmit = e => {
+    e.preventDefault();
+    let lastResponse;
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+        fetch(`${REGISTER}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: JSON.stringify({
+            user_id: 0,
+            username: values.username,
+            password: values.password,
+            roomId: values.roomId[1],
+            email: values.email,
+            firstName: values.firstName,
+            lastName: values.lastName,
+            admin: false
+          })
+        })
+          .then(
+            response => {
+              console.log("response: ", response);
+              console.log("lastResponse: ", lastResponse);
+              lastResponse = response;
+              return response.text();
+            },
+            error => {
+              console.log("Error" + error);
             }
-        });
-    };
+          )
+          .then(text => {
+            console.log("lastResponse in second then:", lastResponse);
+            if (lastResponse.ok) {
+              message.success("Register successfully, please login");
+            } else {
+              message.error("User exists, please login!");
+            }
+          });
+      }
+    });
+  };
 
-    handleConfirmBlur = e => {
-        const { value } = e.target;
-        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-    };
+  handleConfirmBlur = e => {
+    const { value } = e.target;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
 
-    compareToFirstPassword = (rule, value, callback) => {
-        const { form } = this.props;
-        if (value && value !== form.getFieldValue('password')) {
-            callback('Two passwords that you enter is inconsistent!');
-        } else {
-            callback();
-        }
-    };
-
-    validateToNextPassword = (rule, value, callback) => {
-        const { form } = this.props;
-        if (value && this.state.confirmDirty) {
-            form.validateFields(['confirm'], { force: true });
-        }
-        callback();
-    };
-
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        const { autoCompleteResult } = this.state;
-
-        const formItemLayout = {
-            labelCol: {
-                xs: { span: 24 },
-                sm: { span: 8 },
-            },
-            wrapperCol: {
-                xs: { span: 24 },
-                sm: { span: 16 },
-            },
-        };
-        const tailFormItemLayout = {
-            wrapperCol: {
-                xs: {
-                    span: 24,
-                    offset: 0,
-                },
-                sm: {
-                    span: 16,
-                    offset: 8,
-                },
-            },
-        };
-
-
-        return (
-            <Form onSubmit={this.handleSubmit} className="register-form">
-                <Form.Item label="Username">
-                    {getFieldDecorator('username', {
-                        rules: [{ required: true, message: 'Please input your username!', whitespace: true }],
-                    })(<Input />)}
-                </Form.Item>
-                <Form.Item label="E-mail">
-                    {getFieldDecorator('email', {
-                        rules: [
-                            {
-                                type: 'email',
-                                message: 'The input is not valid E-mail!',
-                            },
-                            {
-                                required: true,
-                                message: 'Please input your E-mail!',
-                            },
-                        ],
-                    })(<Input />)}
-                </Form.Item>
-                <Form.Item label="Password" hasFeedback>
-                    {getFieldDecorator('password', {
-                        rules: [
-                            {
-                                required: true,
-                                message: 'Please input your password!',
-                            },
-                            {
-                                validator: this.validateToNextPassword,
-                            },
-                        ],
-                    })(<Input.Password />)}
-                </Form.Item>
-                <Form.Item label="Confirm Password" hasFeedback>
-                    {getFieldDecorator('confirm', {
-                        rules: [
-                            {
-                                required: true,
-                                message: 'Please confirm your password!',
-                            },
-                            {
-                                validator: this.compareToFirstPassword,
-                            },
-                        ],
-                    })(<Input.Password onBlur={this.handleConfirmBlur} />)}
-                </Form.Item>
-                <Form.Item label="Apartment Number">
-                    {getFieldDecorator('roomId', {
-                        // initialValue: ['Place1', 'Place2', 'Place3'],
-                        rules: [
-                            { type: 'array', required: true, message: 'Please select your habitual residence!' },
-                        ],
-                    })(<Cascader options={residences} />)}
-                </Form.Item>
-                <Form.Item label="Phone Number">
-                    {getFieldDecorator('phone', {
-                        rules: [{ required: true, message: 'Please input your phone number!' }],
-                    })(<Input style={{ width: '100%' }} />)}
-                </Form.Item>
-                <Form.Item label="First Name">
-                    {getFieldDecorator('firstName', {
-                        rules: [{required: true, message: 'Please input your first name!'}]
-                    })(<Input />)}
-                </Form.Item>
-                <Form.Item label="Last Name">
-                    {getFieldDecorator('lastName', {
-                        rules: [{required: true, message: 'Please input your first name!'}]
-                    })(<Input />)}
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit">
-                        Register
-                    </Button>
-                </Form.Item>
-            </Form>
-        );
+  compareToFirstPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && value !== form.getFieldValue("password")) {
+      callback("Two passwords that you enter is inconsistent!");
+    } else {
+      callback();
     }
+  };
+
+  validateToNextPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(["confirm"], { force: true });
+    }
+    callback();
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const { autoCompleteResult } = this.state;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 }
+      }
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0
+        },
+        sm: {
+          span: 16,
+          offset: 8
+        }
+      }
+    };
+
+    return (
+      <Form onSubmit={this.handleSubmit} className='register-form'>
+        <h1 className='register-text'>User Register</h1>
+        <Form.Item label='Username'>
+          {getFieldDecorator("username", {
+            rules: [
+              {
+                required: true,
+                message: "Please input your username!",
+                whitespace: true
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label='E-mail'>
+          {getFieldDecorator("email", {
+            rules: [
+              {
+                type: "email",
+                message: "The input is not valid E-mail!"
+              },
+              {
+                required: true,
+                message: "Please input your E-mail!"
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label='Password' hasFeedback>
+          {getFieldDecorator("password", {
+            rules: [
+              {
+                required: true,
+                message: "Please input your password!"
+              },
+              {
+                validator: this.validateToNextPassword
+              }
+            ]
+          })(<Input.Password />)}
+        </Form.Item>
+        <Form.Item label='Confirm Password' hasFeedback>
+          {getFieldDecorator("confirm", {
+            rules: [
+              {
+                required: true,
+                message: "Please confirm your password!"
+              },
+              {
+                validator: this.compareToFirstPassword
+              }
+            ]
+          })(<Input.Password onBlur={this.handleConfirmBlur} />)}
+        </Form.Item>
+        <Form.Item label='Apartment Number'>
+          {getFieldDecorator("roomId", {
+            // initialValue: ['Place1', 'Place2', 'Place3'],
+            rules: [
+              {
+                type: "array",
+                required: true,
+                message: "Please select your habitual residence!"
+              }
+            ]
+          })(<Cascader options={residences} />)}
+        </Form.Item>
+        <Form.Item label='Phone Number'>
+          {getFieldDecorator("phone", {
+            rules: [
+              { required: true, message: "Please input your phone number!" }
+            ]
+          })(<Input style={{ width: "100%" }} />)}
+        </Form.Item>
+        <Form.Item label='First Name'>
+          {getFieldDecorator("firstName", {
+            rules: [
+              { required: true, message: "Please input your first name!" }
+            ]
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label='Last Name'>
+          {getFieldDecorator("lastName", {
+            rules: [
+              { required: true, message: "Please input your first name!" }
+            ]
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item>
+          <Button type='primary' htmlType='submit'>
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
 }
 
-const Register = Form.create({ name: 'register' })(RegistrationForm);
+const Register = Form.create({ name: "register" })(RegistrationForm);
 
 export default Register;
